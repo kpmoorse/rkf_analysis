56f76fc7a15d29cb5e6e2190c7c06936300a7be8
Kellan Moorse <kmoorse@caltech.edu>
2019-03-04 15:26:39 -0800

    Adding standard error bars to multianalysis gain plot

--- rkf_multianalysis.py
+++ rkf_multianalysis.py
@@ -5,6 +5,7 @@ import Tkinter as tk
 import tkFileDialog as tkf
 from tqdm import tqdm
 from ransac import ransac
+from scipy import stats
 
 from rkf_analysis import RkfAnalysis
 
@@ -23,9 +24,9 @@ class RkfMultiAnalysis(object):
         self.inliers = []
 
         self.cgain = np.empty((0, 2))
-        self.mean_gain = np.empty((0, 2))
+        self.stats = np.empty((0, 2))
         self._calc_compound_gain()
-        self._calc_means(robust=False)
+        self._calc_stats(robust=False)
 
     # Instantiate an RkfAnalysis object for each file and extract gain arrays
     def _calc_compound_gain(self, sort=True, rtype='gain'):
@@ -40,10 +41,10 @@ class RkfMultiAnalysis(object):
             self.cgain = self.cgain[np.argsort(self.cgain[:, 0])]
 
     # Calculate the per-frequency gain means with RANSAC outlier-rejection
-    def _calc_means(self, robust=True):
+    def _calc_stats(self, robust=True):
 
         self.inliers = np.array([]).astype(bool)
-        self.mean_gain = np.empty((0, 2))
+        self.stats = np.empty((0, 3))
 
         if ransac:
             meanstd = lambda data: [np.mean(data), np.std(data)]
@@ -55,36 +56,43 @@ class RkfMultiAnalysis(object):
             if robust:
                 mdl = ransac(data, meanstd, azscore, mse, thresh=self.rs_thresh, max_iters=5e4)
                 mean = mdl[0]
+                stderr = stats.sem(data[np.abs(data-mdl[0])/mdl[1] < self.rs_thresh])
                 self.inliers = np.append(self.inliers, np.abs(data-mdl[0])/mdl[1] < self.rs_thresh)
             else:
                 mean = np.nanmean(data)
+                stderr = stats.sem(data)
                 self.inliers = np.append(self.inliers, np.ones(data.shape).astype(bool))
-            self.mean_gain = np.append(self.mean_gain, [[freq, mean]], axis=0)
+            self.stats = np.append(self.stats, [[freq, mean, stderr]], axis=0)
 
     # Plot compound gain array
-    def plot_gain(self, normalize=True):
+    def plot_gain(self, normalize=True, raw=True):
 
         c = plt.rcParams['axes.prop_cycle'].by_key()['color']
         comp = self.cgain.copy()
-        mean = self.mean_gain.copy()
+        stats = self.stats.copy()
 
         # inliers = np.abs(comp[:, 1] - self.rs_mdl[0]) / self.rs_mdl[1] < self.rs_thresh
         if normalize:
-            norm = np.nanmax(self.mean_gain[:, 1])
+            norm = np.nanmax(self.stats[:, 1])
             comp[:, 1] = comp[:, 1] / norm
-            mean[:, 1] = mean[:, 1] / norm
+            stats[:, 1:] = stats[:, 1:] / norm
 
-        plt.plot(comp[self.inliers, 0], comp[self.inliers, 1], '.')
-        plt.plot(comp[~self.inliers, 0], comp[~self.inliers, 1], '.', markerfacecolor='none', c=c[0])
-        plt.plot(mean[:, 0], mean[:, 1], '.-', markersize=12, markerfacecolor='none', c=c[1])
+        if raw:
+            plt.plot(comp[self.inliers, 0], comp[self.inliers, 1], '.')
+            plt.plot(comp[~self.inliers, 0], comp[~self.inliers, 1], '.', markerfacecolor='none', c=c[0])
+        plt.errorbar(stats[:, 0], stats[:, 1], yerr=stats[:, 2],
+                     fmt='.-', capsize=5, markersize=12, markerfacecolor='none', c=c[1])
         maxlim = np.max(comp[self.inliers, 1])
         plt.ylim([maxlim*-0.1, maxlim*1.1])
 
-        plt.title('Post-RANSAC Frequency Response Curve')
-        plt.legend(['Inliers', 'Outliers', 'Model mean'])
+        if np.sum(self.inliers) != comp.shape[0]:
+            plt.title('Post-RANSAC Frequency Response Curve')
+            plt.legend(['Inliers', 'Outliers', 'Model mean'])
+        else:
+            plt.title('Frequency Response Curve')
         plt.xlabel('Frequency (Hz)')
         plt.ylabel('Gain (a.u.)')
 
 
 rkm = RkfMultiAnalysis()
-rkm.plot_gain(normalize=False)
+rkm.plot_gain(normalize=False, raw=False)
